9d1bd24d9b69f49c7c0a66a6d1a3c73f34bc2c86
Ed Conroy <RomanCenturionX>
2015-03-04 16:06:03 -0500

    first commmit

---
+++ Assets/Scripts/Lava.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lava : MonoBehaviour {
+
+	public Transform SpawnPoint;
+
+	void OnCollisionEnter(Collision c)
+	{
+		if(c.gameObject.GetComponent("Player") != null)
+			c.gameObject.transform.position = SpawnPoint.position;
+	}
+}
